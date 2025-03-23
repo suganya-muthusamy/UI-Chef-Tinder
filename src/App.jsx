195ac26d175e306264cf067255905006651a59f7
Suganya Anbazhagan <suganyamuthu9560@gmail.com>
2025-03-22 18:23:54 -0600

    Tailwind, daisy UI configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="">
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </div>
+  );
 }
 
 export default App;
